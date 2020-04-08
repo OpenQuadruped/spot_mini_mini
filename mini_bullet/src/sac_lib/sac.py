17cbbb88547a172b367a0d3b73d16762449dff78
Maurice Rahme <mrahme97@gmail.com>
2020-04-08 12:43:12 -0400

    added cuda compatibility to SAC (using GPU 0 on @beast)

--- mini_bullet/src/sac_lib/sac.py
+++ mini_bullet/src/sac_lib/sac.py
@@ -9,63 +9,78 @@ from torch.distributions import Normal
 from .softQnetwork import SoftQNetwork
 from .valuenetwork import ValueNetwork
 
-class SoftActorCritic(object):
+# device
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    def __init__(self, policy, state_dim, action_dim, replay_buffer,
-                            hidden_dim  = 256,
-                            value_lr    = 3e-4,
-                            soft_q_lr   = 3e-4,
-                            policy_lr   = 3e-4,
-                        ):
+
+class SoftActorCritic(object):
+    def __init__(
+            self,
+            policy,
+            state_dim,
+            action_dim,
+            replay_buffer,
+            hidden_dim=256,
+            value_lr=3e-4,
+            soft_q_lr=3e-4,
+            policy_lr=3e-4,
+    ):
 
         # set up the networks
-        self.value_net        = ValueNetwork(state_dim, hidden_dim)
-        self.target_value_net = ValueNetwork(state_dim, hidden_dim)
-        self.policy_net       = policy
+        self.value_net = ValueNetwork(state_dim, hidden_dim).to(device)
+        self.target_value_net = ValueNetwork(state_dim, hidden_dim).to(device)
+        self.policy_net = policy.to(device)
 
-        self.soft_q_net = SoftQNetwork(state_dim, action_dim, hidden_dim)
+        self.soft_q_net = SoftQNetwork(state_dim, action_dim,
+                                       hidden_dim).to(device)
 
         # copy the target params over
-        for target_param, param in zip(self.target_value_net.parameters(), self.value_net.parameters()):
+        for target_param, param in zip(self.target_value_net.parameters(),
+                                       self.value_net.parameters()):
             target_param.data.copy_(param.data)
 
         # set the losses
-        self.value_criterion  = nn.MSELoss()
+        self.value_criterion = nn.MSELoss()
         self.soft_q_criterion = nn.MSELoss()
 
         # set the optimizers
-        self.value_optimizer  = optim.Adam(self.value_net.parameters(),  lr=value_lr)
-        self.soft_q_optimizer = optim.Adam(self.soft_q_net.parameters(), lr=soft_q_lr)
-        self.policy_optimizer = optim.Adam(self.policy_net.parameters(), lr=policy_lr)
+        self.value_optimizer = optim.Adam(self.value_net.parameters(),
+                                          lr=value_lr)
+        self.soft_q_optimizer = optim.Adam(self.soft_q_net.parameters(),
+                                           lr=soft_q_lr)
+        self.policy_optimizer = optim.Adam(self.policy_net.parameters(),
+                                           lr=policy_lr)
 
         # reference the replay buffer
         self.replay_buffer = replay_buffer
 
-        self.log = {'value_loss' :[], 'q_value_loss':[], 'policy_loss' :[]}
+        self.log = {'value_loss': [], 'q_value_loss': [], 'policy_loss': []}
 
-    def soft_q_update(self, batch_size,
-                            gamma       = 0.99,
-                            mean_lambda = 1e-3,
-                            std_lambda  = 1e-3,
-                            z_lambda    = 0.0,
-                            soft_tau    = 1e-2
-                      ):
-        state, action, reward, next_state, done = self.replay_buffer.sample(batch_size)
+    def soft_q_update(self,
+                      batch_size,
+                      gamma=0.99,
+                      mean_lambda=1e-3,
+                      std_lambda=1e-3,
+                      z_lambda=0.0,
+                      soft_tau=1e-2):
+        state, action, reward, next_state, done = self.replay_buffer.sample(
+            batch_size)
 
-        state      = torch.FloatTensor(state)
-        next_state = torch.FloatTensor(next_state)
-        action     = torch.FloatTensor(action)
-        reward     = torch.FloatTensor(reward).unsqueeze(1)
-        done       = torch.FloatTensor(np.float32(done)).unsqueeze(1)
+        state = torch.FloatTensor(state).to(device)
+        next_state = torch.FloatTensor(next_state).to(device)
+        action = torch.FloatTensor(action).to(device)
+        reward = torch.FloatTensor(reward).unsqueeze(1).to(device)
+        done = torch.FloatTensor(np.float32(done)).unsqueeze(1).to(device)
 
         expected_q_value = self.soft_q_net(state, action)
-        expected_value   = self.value_net(state)
-        new_action, log_prob, z, mean, log_std = self.policy_net.evaluate(state)
-
+        expected_value = self.value_net(state)
+        new_action, log_prob, z, mean, log_std = self.policy_net.evaluate(
+            state)
 
         target_value = self.target_value_net(next_state)
         next_q_value = reward + (1 - done) * gamma * target_value
-        q_value_loss = self.soft_q_criterion(expected_q_value, next_q_value.detach())
+        q_value_loss = self.soft_q_criterion(expected_q_value,
+                                             next_q_value.detach())
 
         expected_new_q_value = self.soft_q_net(state, new_action)
         next_value = expected_new_q_value - log_prob
@@ -74,10 +89,9 @@ class SoftActorCritic(object):
         log_prob_target = expected_new_q_value - expected_value
         policy_loss = (log_prob * (log_prob - log_prob_target).detach()).mean()
 
-
         mean_loss = mean_lambda * mean.pow(2).mean()
-        std_loss  = std_lambda  * log_std.pow(2).mean()
-        z_loss    = z_lambda    * z.pow(2).sum(1).mean()
+        std_loss = std_lambda * log_std.pow(2).mean()
+        z_loss = z_lambda * z.pow(2).sum(1).mean()
 
         policy_loss += mean_loss + std_loss + z_loss
 
@@ -93,11 +107,10 @@ class SoftActorCritic(object):
         policy_loss.backward()
         self.policy_optimizer.step()
 
-
-        for target_param, param in zip(self.target_value_net.parameters(), self.value_net.parameters()):
-            target_param.data.copy_(
-                target_param.data * (1.0 - soft_tau) + param.data * soft_tau
-            )
+        for target_param, param in zip(self.target_value_net.parameters(),
+                                       self.value_net.parameters()):
+            target_param.data.copy_(target_param.data * (1.0 - soft_tau) +
+                                    param.data * soft_tau)
 
         self.log['q_value_loss'].append(q_value_loss.item())
         self.log['value_loss'].append(value_loss.item())
