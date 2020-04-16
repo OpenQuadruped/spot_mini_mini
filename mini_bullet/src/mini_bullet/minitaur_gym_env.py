951d800fb14ab7f1c60bfe141f1becad2f9c3b1b
Maurice Rahme <mrahme97@gmail.com>
2020-04-15 23:43:08 -0400

    fixed typo in reward

--- mini_bullet/src/mini_bullet/minitaur_gym_env.py
+++ mini_bullet/src/mini_bullet/minitaur_gym_env.py
@@ -60,7 +60,7 @@ class MinitaurBulletEnv(gym.Env):
             shake_weight=0.0,
             drift_weight=1.0,
             rp_weight=0.0,
-            rate_weight=0.1,
+            rate_weight=0.3,
             distance_limit=float("inf"),
             observation_noise_stdev=0.0,
             self_collision_enabled=True,
@@ -397,16 +397,15 @@ class MinitaurBulletEnv(gym.Env):
 
         # POSITIVE FOR FORWARD, NEGATIVE FOR BACKWARD | NOTE: HIDDEN
         fwd_speed = self.minitaur.prev_lin_twist[0]
-        print("FORWARD SPEED: {}".format(fwd_speed))
+        # print("FORWARD SPEED: {}".format(fwd_speed))
         desired_speed = 0.5
 
-        # f(x)=-(x-(1)/(desired))^(2)*(desired^2)+1
+        # f(x)=-(x-desired))^(2)*((1/desired)^2)+1
         # to make sure that at 0vel there is 0 reawrd.
         # also squishes allowable tolerance
-        forward_reward = (-(fwd_speed -
-                            (1.0 / desired_speed))**2) * (desired_speed**
-                                                          2) + 1.0
-        if forward_reward < 0.0:
+        forward_reward = (-(fwd_speed - (desired_speed))**2) * (
+            (1.0 / desired_speed)**2) + 1.0
+        if forward_reward < 0.0 and fwd_speed < 0.0:
             forward_reward = 0.0
 
         forward_reward = fwd_speed
