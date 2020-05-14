bdb184edd254b3c3e1b4db3cb865147a573b3968
Maurice Rahme <mrahme97@gmail.com>
2020-05-14 16:57:52 -0400

    removed artefacts of TG v1

--- mini_bullet/src/tg_lib/traj_gen.py
+++ mini_bullet/src/tg_lib/traj_gen.py
@@ -4,24 +4,7 @@ import numpy as np
 class CyclicIntegrator():
     def __init__(self, dphi_leg=0.0):
         # Phase
-        self.phi = 0.0
-        self.dphi_leg = dphi_leg
-        self.phi_leg = np.fmod(self.phi + self.dphi_leg, 2 * np.pi)
         self.tprime = 0.0
-
-    def progress_phase(self, f_tg, dt):
-        """ Eqn 1 in paper appendix
-
-            dt: timestep
-
-            POLICY PARAMS:
-                f_tg frequency of TG
-        """
-        self.phi += np.fmod(2 * np.pi * f_tg * dt, 2 * np.pi)
-
-    def progress_leg_phase(self, f_tg, dt):
-        self.progress_phase(f_tg, dt)
-        self.phi_leg = np.fmod(self.phi + self.dphi_leg, 2 * np.pi)
 
     def progress_tprime(self, dt, f_tg, swing_stance_speed_ratio):
         """ swing_stance_speed_ratio is Beta in the paper
@@ -33,14 +16,6 @@ class CyclicIntegrator():
             The speed of the phase depends on swing vs stance phase
             (phase > np.pi or phase < np.pi)  which has different speeds.
         """
-        # self.progress_leg_phase(f_tg, dt)
-
-        # if self.phi_leg > 0.0 and self.phi_leg < 2.0 * np.pi * swing_stance_speed_ratio:
-        #     self.tprime = self.phi_leg / (2.0 * (1 - swing_stance_speed_ratio))
-        # else:
-        #     self.tprime = 2.0 * np.pi - (2 * np.pi - self.phi_leg) / (
-        #         2.0 * swing_stance_speed_ratio)
-
         stance_speed_coef = (swing_stance_speed_ratio +
                              1) * f_tg / swing_stance_speed_ratio
         swing_speed_coef = (swing_stance_speed_ratio + 1) * f_tg
