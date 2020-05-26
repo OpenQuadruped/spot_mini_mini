fixed stopping phase incr. was getting tiny numerical error, so loosened condition for open-loop touchdown to >= 0.998 instead of >= 1.0 for the swing phase

bullet/src/spotmicro/util/gui.py
@@ -31,7 +31,7 @@ class GUI:
         self.LateralFractionId = pb.addUserDebugParameter(
             "Lateral Fraction", -np.pi / 2.0, np.pi / 2.0, 0.)
         self.StepVelocityId = pb.addUserDebugParameter("Step Velocity", 0.1,
-                                                       3., 0.5)
+                                                       3., 0.1)
 
         self.quadruped = quadruped
 