1a0ae4c98d18c4558cc235c2419a6cbc6947b013
Maurice Rahme <mrahme97@gmail.com>
2020-06-09 01:28:46 -0400

    VER IMPORTANT: re-added incorrect phase inspection method but commented out. MUST uncomment for ARS299 policy to work as the erroneous phases were part of the observation space

--- spotmicro/spot.py
+++ spotmicro/spot.py
@@ -667,6 +667,19 @@ class Spot(object):
         self.YawRate = YawRate
 
     def GetLegPhases(self, TrajectoryGenerator):
+        """ Leg phases according to TG from 0->2
+            0->1: Stance
+            1->2 Swing
+        """
+        # NOTE: IMPORTANT: UNCOMMENT THIS TO USE ARS POLICY 299 AS IT DEPENDS
+        # ON THE 'INCORRECT' LEG PHASE MEASUREMENTS!!!!!!
+        # if self.StepVelocity != 0.0:
+        #     Tswing = 2.0 * abs(self.StepLength) / abs(self.StepVelocity)
+        # else:
+        #     Tswing = 0.0
+        # for i in range(4):
+        #     TrajectoryGenerator.GetPhase(i, TrajectoryGenerator.Tstance,
+        #                                  Tswing)
         self.LegPhases = TrajectoryGenerator.Phases
 
     def GetExternalObservations(self, TrajectoryGenerator, controller):
