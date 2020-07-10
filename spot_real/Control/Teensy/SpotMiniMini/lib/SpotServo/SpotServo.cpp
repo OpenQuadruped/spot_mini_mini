2c91d383d30595994eb510fccb9af9172adf8100
Maurice Rahme <mrahme97@gmail.com>
2020-07-10 12:30:12 -0400

    servo calibration is working.

--- spot_real/Control/Teensy/SpotMiniMini/lib/SpotServo/SpotServo.cpp
+++ spot_real/Control/Teensy/SpotMiniMini/lib/SpotServo/SpotServo.cpp
@@ -5,13 +5,16 @@ using namespace std;
 // Spot Full Constructor
 void SpotServo::Initialize(const int & servo_pin, const double & home_angle_, const double & offset_, const LegType & leg_type_, const JointType & joint_type_)
 {
-	servo.attach(servo_pin, 500, 2500);
+	conv_slope = (max_pwm - min_pwm) / (control_range - 0.0);
+	conv_intcpt = max_pwm - conv_slope * control_range;
+	servo.attach(servo_pin, min_pwm, max_pwm);
 	offset = offset_;
 	home_angle = home_angle_;
 	current_pose = home_angle + offset;
 	leg_type = leg_type_;
 	joint_type = joint_type;
-	servo.write((home_angle + offset) * 180.0 / control_range);
+	int pwm = (home_angle + offset) * conv_slope + conv_intcpt;
+	servo.writeMicroseconds(pwm);
 	delay(1000);
 	last_actuated = millis();
 }
@@ -43,7 +46,8 @@ void SpotServo::update_clk()
 			// returns 1.0 * sign of goal_pose - current_pose
 			double direction = copysign(1.0, goal_pose - current_pose);
 			current_pose += direction * (wait_time / 1000.0) * desired_speed;
-			servo.write(current_pose * 180.0 / control_range);
+			int pwm = (current_pose) * conv_slope + conv_intcpt;
+			servo.writeMicroseconds(pwm);
 			last_actuated = millis();
 		}
 	}
