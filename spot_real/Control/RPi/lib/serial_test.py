069f789dc92f674f44ec43db14fe94a54d1e0253
Maurice Rahme <mrahme97@gmail.com>
2020-07-09 23:07:33 -0400

    more interactive test script

--- spot_real/Control/RPi/lib/serial_test.py
+++ spot_real/Control/RPi/lib/serial_test.py
@@ -4,8 +4,10 @@ from Teensy_Interface import TeensyInterface
 
 ti = TeensyInterface()
 while True:
+    input("Send?")
     ti.add_to_buffer(4, 0, 135, 60)
     ti.send_buffer()
     time.sleep(1.0)
+    input("Read?")
     print(ti.read_buffer())
     time.sleep(1.0)
