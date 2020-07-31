0ecae1a0b04b2d4caa183df174611cffa04e7bf4
Maurice Rahme <mrahme97@gmail.com>
2020-07-31 15:54:04 -0400

    updated angle biases. next update is offsets
    Former-commit-id: a1b69e1bce420b1d9daf6798ed289c739c7f7386

--- spot_real/Control/Teensy/SpotMiniMini/lib/Utilities/Utilities.hpp
+++ spot_real/Control/Teensy/SpotMiniMini/lib/Utilities/Utilities.hpp
@@ -2,13 +2,11 @@
 #define UTILITIES_INCLUDE_GUARD_HPP
 #include "SpotServo.hpp"
 /// \file
-/// \brief Utilities Library. Adapted from https://github.com/adham-elarabawy/OpenQuadruped
+/// \brief Utilities Library.
 class Utilities {
   public:
-  void upper(char* s);
   double angleConversion(double angle, double home_angle, LegType legtype, JointType joint_type);
   double toDegrees(double radianVal);
-  double max(double a0, double a1, double a2);
 };
 
 #endif
