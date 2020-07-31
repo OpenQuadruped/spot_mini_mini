e29c33d39d39faa2e808b23a4de2fb0a50150c4f
Maurice Rahme <mrahme97@gmail.com>
2020-07-30 22:18:18 -0400

    draft 1 of rosserial interface

---
+++ spot_real/Control/Teensy/SpotMiniMini/lib/ROSSerial/ROSSerial.hpp
@@ -0,0 +1,156 @@
+#ifndef ROSSERIAL_HPP
+#define ROSSERIAL_HPP
+
+#include <ros.h>
+#include <ros/time.h>
+#include <Arduino.h>
+#include <mini_ros/ContactData.h>
+#include <mini_ros/IMUdata.h>
+#include <mini_ros/JointAngles.h>
+#include <SpotServo.hpp>
+#include <IMU.hpp>
+
+struct LegJoints
+{
+    LegType legtype;
+    double shoulder = 0.0;
+    double elbow = 0.0;
+    double wrist = 0.0;
+
+    LegJoints(const LegType & legtype_)
+    {
+        legtype = legtype_;
+    }
+
+    void FillLegJoint(const double & s, const double & e, const double & w)
+    {
+        shoulder = s;
+        elbow = e;
+        wrist = w;
+    }
+};
+
+namespace ros_srl
+{
+
+    class ROSSerial
+    {
+        // Node Handle
+        ros::NodeHandle nh_;
+
+        // Joint Angle Subscriber
+        ros::Subscriber<mini_ros::JointAngles, ROSSerial> ja_sub_;
+        // joint msg timer
+        unsigned long prev_joints_time_;
+        unsigned long prev_resetter_time_;
+        // joint msg flag
+        bool joints_cmd_active_ = false;
+        // joint msgs
+        LegType fl_leg = FL;
+        LegType fr_leg = FR;
+        LegType bl_leg = BL;
+        LegType br_leg = BR;
+        LegJoints FL_ = LegJoints(fl_leg);
+        LegJoints FR_ = LegJoints(fr_leg);
+        LegJoints BL_ = LegJoints(bl_leg);
+        LegJoints BR_ = LegJoints(br_leg);
+
+        // IMU msg and publisher
+        mini_ros::IMUdata imu_msg_;
+        ros::Publisher imu_pub_;
+
+        // Contact msg and publisher
+        mini_ros::ContactData contact_msg_;
+        ros::Publisher contact_pub_;
+
+        void JointCommandCallback(const mini_ros::JointAngles& ja_msg)
+        {
+            prev_joints_time_ = micros();
+
+            FL_.FillLegJoint(ja_msg.fls, ja_msg.fle, ja_msg.flw);
+            FR_.FillLegJoint(ja_msg.frs, ja_msg.fre, ja_msg.frw);
+            BL_.FillLegJoint(ja_msg.bls, ja_msg.ble, ja_msg.blw);
+            BR_.FillLegJoint(ja_msg.brs, ja_msg.bre, ja_msg.brw);
+        }
+
+        public:
+            ROSSerial():
+                ja_sub_("spot/joints", &ROSSerial::JointCommandCallback, this),
+                imu_pub_("spot/imu", &imu_msg_),
+                contact_pub_("spot/contact", &contact_msg_),
+                joints_cmd_active_(false)
+
+            {
+                nh_.initNode();
+                nh_.getHardware()->setBaud(500000);
+
+                nh_.subscribe(ja_sub_);
+
+                nh_.advertise(imu_pub_);
+                nh_.advertise(contact_pub_);
+
+                nh_.loginfo("SPOT MINI MINI ROS CLIENT CONNECTED");
+            }
+
+            void returnJoints(LegJoints & FL_ref,  LegJoints & FR_ref, LegJoints & BL_ref, LegJoints & BR_ref)
+            {
+                FL_ref = FL_;
+                FR_ref = FR_;
+                BL_ref = BL_;
+                BR_ref = BR;        
+            }
+
+            bool jointsInputIsActive()
+            {
+                return joints_cmd_active_;
+            }
+
+            void run()
+            {   
+                unsigned long now = micros();
+                if((now - prev_resetter_time_) > 333333)
+                {
+                    prev_resetter_time_ = now;
+
+                    if((now - prev_joints_time_) < 500000)
+                    {
+                        joints_cmd_active_ = true;
+                    }
+                    else
+                    {
+                        joints_cmd_active_ = false;
+                    }
+                }
+                nh_.spinOnce();
+            }
+
+            void publishContacts(const bool & FLC, const bool & FRC, const bool & BLC, const bool & BRC)
+            {
+                contact_msg_.FL = FLC;
+                contact_msg_.FR = FRC;
+                contact_msg_.BL = BLC;
+                contact_msg_.BR = BRC;
+
+                contact_pub_.publish(&contact_msg_);
+            }
+
+            void publishIMU(imu::Vector<3> eul, imu::Vector<3> acc, imu::Vector<3> gyro)
+            {
+                // NOTE: switching eul.x and eul.x because Bosch is weird..
+                imu_msg_.roll = eul.z();
+                imu_msg_.pitch = eul.y();
+
+                imu_msg_.acc_x = acc.x();
+                imu_msg_.acc_y = acc.y();
+                imu_msg_.acc_z = acc.z();
+
+                imu_msg_.gyro_x = gyro.x();
+                imu_msg_.gyro_y = gyro.y();
+                imu_msg_.gyro_z = gyro.z();
+
+                imu_pub_.publish(&imu_msg_);
+            }
+    };
+}
+
+#endif
--- spot_real/Control/Teensy/SpotMiniMini/src/main.cpp
+++ spot_real/Control/Teensy/SpotMiniMini/src/main.cpp
@@ -2,25 +2,20 @@
 /// \brief Teensy Main. Adapted from https://github.com/adham-elarabawy/OpenQuadruped
 
 #include <Arduino.h>
-#include "SpotServo.hpp"
-#include "ContactSensor.hpp"
-#include "Kinematics.hpp"
-#include "Utilities.hpp"
-#include "IMU.hpp"
+#include <SpotServo.hpp>
+#include <ContactSensor.hpp>
+#include <Kinematics.hpp>
+#include <Utilities.hpp>
+#include <IMU.hpp>
 #include <Servo.h>
+#include <ROSSerial.hpp>
 
 #define DEBUGSERIAL Serial
 
-using namespace std;
-
-String serialResponse = "";
-// LegNum, FootX, FootY, FootZ
-char msg0[] = "0,-999.9,-999.9,-999.9"; // structure for position command
-// LegNum, FootX, FootY, FootZ, FootSpeedX, FootSpeedY, FootSpeedZ
-// char msg1[] = "0,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9"; // structure for position/speed command
 bool ESTOPPED = false;
 int max_speed = 1000; // doesn't really mean anything, theoretically deg/sec
 double last_estop = millis();
+static unsigned long prev_publish_time;
 const int ledPin = 13;
 
 
@@ -33,6 +28,15 @@ SpotServo * AllServos[12] = {&FL_Shoulder, &FR_Shoulder, &BL_Shoulder, &BR_Shoul
 Utilities util;
 Kinematics ik;
 IMU imu_sensor;
+LegType fl_leg = FL;
+LegType fr_leg = FR;
+LegType bl_leg = BL;
+LegType br_leg = BR;
+LegJoints FL_ = LegJoints(fl_leg);
+LegJoints FR_ = LegJoints(fr_leg);
+LegJoints BL_ = LegJoints(bl_leg);
+LegJoints BR_ = LegJoints(br_leg);
+ros_srl::ROSSerial ros_serial;
 
 void detach_servos()
 {
@@ -76,6 +80,49 @@ void update_servos()
     BR_Wrist.update_clk();
 }
 
+void command_servos(const LegJoints & legjoint)
+{
+  int leg = -1;
+
+  if (legjoint.legtype == FL)
+  {
+    leg = 0;
+  } else if (legjoint.legtype == FR)
+  {
+    leg = 1;
+  } else if (legjoint.legtype == BL)
+  {
+    leg = 2;
+  } else if (legjoint.legtype == BR)
+  {
+    leg = 3;
+  }
+
+
+  double shoulder_home = (*Shoulders[leg]).return_home();
+  double elbow_home = (*Elbows[leg]).return_home();
+  double wrist_home = (*Wrists[leg]).return_home();
+
+  double Shoulder_angle = util.angleConversion(legjoint.shoulder, shoulder_home, legjoint.legtype, Shoulder);
+  double Elbow_angle = util.angleConversion(legjoint.elbow, elbow_home, legjoint.legtype, Elbow);
+  double Wrist_angle = util.angleConversion(legjoint.wrist, wrist_home, legjoint.legtype, Wrist);
+
+  double h_dist = abs(Shoulder_angle - (*Shoulders[leg]).GetPoseEstimate());
+  double s_dist = abs(Elbow_angle - (*Elbows[leg]).GetPoseEstimate());
+  double w_dist = abs(Wrist_angle - (*Wrists[leg]).GetPoseEstimate());
+
+  double scaling_factor = util.max(h_dist, s_dist, w_dist);
+
+  h_dist /= scaling_factor;
+  s_dist /= scaling_factor;
+  w_dist /= scaling_factor;
+
+  (*Shoulders[leg]).SetGoal(Shoulder_angle, max_speed * h_dist);
+  (*Elbows[leg]).SetGoal(Elbow_angle, max_speed * s_dist);
+  (*Wrists[leg]).SetGoal(Wrist_angle, max_speed * w_dist);
+  
+}
+
 void update_sensors()
 {
     FL_sensor.update_clk();
@@ -86,14 +133,6 @@ void update_sensors()
 
 // THIS ONLY RUNS ONCE
 void setup() {
-  // HARDWARE - PI COMM
-  Serial1.begin(500000);
-  while (!Serial1);
-  // DEBUG - USB
-  // Serial.begin(9600);
-  // while (!Serial);
-
-  // above code waits until serial1 and serial ready
 
   pinMode(ledPin, OUTPUT);
 
@@ -140,7 +179,7 @@ void setup() {
 
   last_estop = millis();
 
-  Serial1.print("READY!\n");
+  prev_publish_time = micros();
 }
 
 // THIS LOOPS FOREVER
@@ -148,94 +187,21 @@ void loop()
 {
   // CHECK SENSORS AND SEND INFO
   // CONTACT
-  char FL_sensor_buf[2];
-  char FR_sensor_buf[2];
-  char BL_sensor_buf[2];
-  char BR_sensor_buf[2];
-  char contact_sensor_buf[20];
-
-  if (FL_sensor.isTriggered())
+  if ((micros() - prev_publish_time) >= 20000)
   {
-    // integer, buffer, base
-    itoa(1, FL_sensor_buf, 10);
-  } else
-  {
-    itoa(0, FL_sensor_buf, 10);
-  }
+    ros_serial.publishContacts(FL_sensor.isTriggered(), FR_sensor.isTriggered(), BL_sensor.isTriggered(), BR_sensor.isTriggered());
 
-  if (FR_sensor.isTriggered())
-  {
-    itoa(1, FR_sensor_buf, 10);
-  } else
-  {
-    itoa(0, FR_sensor_buf, 10);
-  }
-  if (BL_sensor.isTriggered())
-  {
-    itoa(1, BL_sensor_buf, 10);
-  } else
-  {
-    itoa(0, BL_sensor_buf, 10);
-  }
-  if (BR_sensor.isTriggered())
-  {
-    itoa(1, BR_sensor_buf, 10);
-  } else
-  {
-    itoa(0, BR_sensor_buf, 10);
-  }
+    //IMU
+    if (imu_sensor.available())
+    {
+      imu::Vector<3> eul = imu_sensor.GetEuler();
+      imu::Vector<3> acc = imu_sensor.GetAcc();
+      imu::Vector<3> gyro = imu_sensor.GetGyro();
 
-  // convert all to string
-  sprintf_P(contact_sensor_buf, PSTR("CONTACT,%s,%s,%s,%s\n"), FL_sensor_buf, FR_sensor_buf, BL_sensor_buf, BR_sensor_buf);
+      ros_serial.publishIMU(eul, acc, gyro);
+    }
 
-  // Send to RPI
-  Serial1.print(contact_sensor_buf);
-
-  // DEBUG
-  // DEBUGSERIAL.print(contact_sensor_buf);
-
-  //IMU
-  if (imu_sensor.available())
-  {
-    char roll_buf[] = "-9.999";
-    char pitch_buf[] = "-9.999";
-    char yaw_buf[] = "-9.999";
-    char acc_x_buf[] = "-999.99";
-    char acc_y_buf[] = "-999.99";
-    char acc_z_buf[] = "-999.99";
-    char gyro_x_buf[] = "-999.99";
-    char gyro_y_buf[] = "-999.99";
-    char gyro_z_buf[] = "-999.99";
-    // just in case we get some big values
-    char imu_buf[] = "IMUDATA,-9.999,-9.999,-999.99,-999.99,-999.99,-999.99,-999.99,-999.99";
-
-    imu::Vector<3> eul = imu_sensor.GetEuler();
-    // val, width, precision, buff
-    // NOTE: switching eul.x and eul.x because Bosch is weird..
-    dtostrf(eul.z(), 0, 2, roll_buf);
-    dtostrf(eul.y(), 0, 2, pitch_buf);
-    dtostrf(eul.x(), 0, 2, yaw_buf);
-    imu::Vector<3> acc = imu_sensor.GetAcc();
-    dtostrf(acc.x(), 0, 2, acc_x_buf);
-    dtostrf(acc.y(), 0, 2, acc_y_buf);
-    dtostrf(acc.z(), 0, 2, acc_z_buf);
-    imu::Vector<3> gyro = imu_sensor.GetGyro();
-    dtostrf(gyro.x(), 0, 2, gyro_x_buf);
-    dtostrf(gyro.y(), 0, 2, gyro_y_buf);
-    dtostrf(gyro.z(), 0, 2, gyro_z_buf);
-
-    // convert all to string
-    sprintf_P(imu_buf, PSTR("IMUDATA,%s,%s,%s,%s,%s,%s,%s,%s\n"), roll_buf, pitch_buf, acc_x_buf, acc_y_buf, acc_z_buf, gyro_x_buf, gyro_y_buf, gyro_z_buf);
-
-    // sprintf_P(imu_buf, PSTR("Roll: %s,\tPitch: %s,\tYaw: %s\n"), roll_buf, pitch_buf, yaw_buf);
-
-
-    // Send to RPI
-    Serial1.print(imu_buf);
-
-    // DEBUG
-    // DEBUGSERIAL.print(imu_buf);
-
+    prev_publish_time = micros();
   }
 
   if(!ESTOPPED){
@@ -244,166 +210,14 @@ void loop()
     detach_servos();
   }
   update_sensors();
-  if (Serial1.available() > 0)
-  {
-    serialResponse = Serial1.readStringUntil('\n');
-    // Serial1.print(serialResponse);
-    // Convert from String Object to String.
-    // NOTE: Must have size of msg0
-    char buf[sizeof(msg0)];
-    serialResponse.toCharArray(buf, sizeof(buf));
-    char *ptr = buf;
-    char *str;
-    int message_string_index = 0;
-    int leg = -1; //0=FL, 1=FR, 2=BL, 3=BR
-    double x = -9999;
-    double y = -9999;
-    double z = -9999;
-    // For Servo Calibration Request
-    int servo_num = -1;
-    double servo_calib_angle = 135.0;
-    while ((str = strtok_r(ptr, ",", &ptr)) != NULL) // delimiter is the comma
-    {
-      if((strcmp(str, "e") == 0 or strcmp(str, "E") == 0) and last_estop > 200)
-      {
-        // TRIGGER
-        ESTOPPED = !ESTOPPED;
-        last_estop = millis();
-      }
+  
+  // Command Servos
+  ros_serial.returnJoints(FL_, FR_, BL_, BR_);
+  command_servos(FL_);
+  command_servos(FR_);
+  command_servos(BL_);
+  command_servos(BR_);
 
-      // Read Desired Leg
-      if(message_string_index == 0)
-      {
-        // between 0 and 4
-        leg = atoi(str);
-        // Serial1.print(leg);
-        if (leg < 0 or leg > 4)
-        {
-          leg = -1;
-        }
-      }
-
-      if (leg >= 0 and leg <=3)
-      {
-        // Read Desired Foot x pos
-        if (message_string_index == 1)
-        {
-          x = atof(str);
-          // Serial1.print(x);
-        } else if (message_string_index == 2)
-        {
-          // Read Desired Foot y pos
-          y = atof(str);
-          // Serial1.print(y);
-        } else if (message_string_index == 3)
-        {
-          // Read Desired Foot z pos
-          z = atof(str);
-          // Serial1.print(z);
-        }
-      } else if (leg == 4)
-      {
-        // CALIBRATION REQUEST
-        if(message_string_index == 1)
-        {
-          servo_num = atoi(str);
-          // only one of 12 servos
-          x = 0;
-          y = 0;
-          z = 0;
-        }  else if (message_string_index == 2)
-        {
-          // Read Desired Foot y pos
-          servo_calib_angle = atof(str);
-        }
-      }
-
-      // Increment message message_string_index
-      message_string_index++;
-    }
-
-    //COMPLETE MESSAGE CHECK
-    if(leg != -9999 and x != -9999 and y != -9999 and z != -9999){
-      // Serial.print("complete message");
-
-      if (servo_num == -1 and leg != -1)
-      // NORMAL OPERATION
-      {
-        // DEBUGSERIAL.print("SERVO ACT\n");
-
-        LegType leg_type;
-        LegQuadrant legquad;
-        if (leg == 0 or leg == 2)
-        {
-          legquad = Left;
-          if (leg == 0)
-          {
-            leg_type = FL;
-          } else
-          {
-            leg_type = BL;
-          }
-        } else
-        {
-          legquad = Right;
-          if (leg == 1)
-          {
-            leg_type = FR;
-          } else
-          {
-            leg_type = BR;
-          }
-        }
-
-        double shoulder_home = (*Shoulders[leg]).return_home();
-        double elbow_home = (*Elbows[leg]).return_home();
-        double wrist_home = (*Wrists[leg]).return_home();
-
-        // NOTE: x,y,z maps to shoulder, elbow wrist... trying smth new
-
-
-        double Shoulder_angle = util.angleConversion(x, shoulder_home, leg_type, Shoulder);
-        double Elbow_angle = util.angleConversion(y, elbow_home, leg_type, Elbow);
-        double Wrist_angle = util.angleConversion(z, wrist_home, leg_type, Wrist);
-
-        double h_dist = abs(Shoulder_angle - (*Shoulders[leg]).GetPoseEstimate());
-        double s_dist = abs(Elbow_angle - (*Elbows[leg]).GetPoseEstimate());
-        double w_dist = abs(Wrist_angle - (*Wrists[leg]).GetPoseEstimate());
-
-        double scaling_factor = util.max(h_dist, s_dist, w_dist);
-
-        h_dist /= scaling_factor;
-        s_dist /= scaling_factor;
-        w_dist /= scaling_factor;
-
-        (*Shoulders[leg]).SetGoal(Shoulder_angle, max_speed * h_dist);
-        (*Elbows[leg]).SetGoal(Elbow_angle, max_speed * s_dist);
-        (*Wrists[leg]).SetGoal(Wrist_angle, max_speed * w_dist);
-
-        // SEND DEBUG DATA
-        char Shoulder_buf[10];
-        char Elbow_buf[10];
-        char Wrist_buf[10];
-        char Leg_buf[10];
-        char Debug_buf[512];
-
-        itoa(leg, Leg_buf, 10);
-        dtostrf(Shoulder_angle, 0, 4, Shoulder_buf);
-        dtostrf(Elbow_angle, 0, 4, Elbow_buf);
-        dtostrf(Wrist_angle, 0, 4, Wrist_buf);
-
-        // convert all to string
-        // sprintf_P(Debug_buf, PSTR("Leg: %s \t Shoulder: %s \t Elbow: %s \t Wirst:%s\n"), Leg_buf, Shoulder_buf, Elbow_buf, Wrist_buf);
-
-        // Send to RPI
-        // Serial1.print(Debug_buf);
-
-      } else if (servo_num != -1)
-      {
-        // SERVO CALIBRATION - SEND ANGLE DIRECTLY
-        // DEBUGSERIAL.print("SERVO CALIB\n");
-        (*AllServos[servo_num]).SetGoal(servo_calib_angle, max_speed);
-      }
-    }
-  }
+  // Update ROS Node (spinOnce etc...)
+  ros_serial.run();
 }
