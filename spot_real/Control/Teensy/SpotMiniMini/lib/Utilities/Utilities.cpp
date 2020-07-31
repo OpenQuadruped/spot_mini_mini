3442e849725086497150d0eb58bf8796087cced1
Maurice Rahme <mrahme97@gmail.com>
2020-07-31 16:12:03 -0400

    added offets

--- spot_real/Control/Teensy/SpotMiniMini/lib/Utilities/Utilities.cpp
+++ spot_real/Control/Teensy/SpotMiniMini/lib/Utilities/Utilities.cpp
@@ -5,6 +5,19 @@ double Utilities::toDegrees(double radianVal) {
   return radianVal * 57296 / 1000.0;
 }
 
+double Utilities::max(double a0, double a1, double a2) {
+  if(a0 >= a1 && a0 >= a2) {
+    return a0;
+  }
+  if(a1 >= a0 && a1 >= a2) {
+    return a1;
+  }
+  if(a2 >= a1 && a2 >= a0) {
+    return a2;
+  }
+}
+
+
 double Utilities::angleConversion(double angle, double home_angle, LegType legtype, JointType joint_type) {
   double mod_angle = home_angle;
 
